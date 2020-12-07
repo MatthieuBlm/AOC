dec3c0f5d98319f6e786b99afa724d79ca683e7e
MatthieuBlm <matthieu.bellamy@hotmail.fr>
2020-12-07 09:20:38 +0100

    Starting of day 7

---
+++ src/main/java/com/matthieu/aoc_2020/service/resolver/Resolver7p1.java
@@ -0,0 +1,65 @@
+package com.matthieu.aoc_2020.service.resolver;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import com.matthieu.aoc_2020.exception.PrepareDataException;
+import com.matthieu.aoc_2020.exception.SolveException;
+import com.matthieu.aoc_2020.model.travel_bag.BagRule;
+
+public class Resolver7p1 implements Resolver {
+
+	protected List<BagRule> rules;
+	protected Map<String, BagRule> mapRules;
+	protected List<String> canContainSearchingColorBag;
+	protected String searchingColor;
+	
+	@Override
+	public void prepareData(List<String> values) throws PrepareDataException {
+		this.rules = values.stream().map(BagRule::new).collect(Collectors.toList());
+		this.mapRules = rules.stream().collect(Collectors.toMap(BagRule::getColor, bagRule -> bagRule));
+		this.canContainSearchingColorBag = new ArrayList<>();
+		this.searchingColor = "shiny gold";
+	}
+
+	@Override
+	public boolean solve() throws SolveException {
+		
+		for (BagRule bagRule : rules) {
+			if(bagRule.getColor().equals(searchingColor)) 
+				continue;
+			
+			List<BagRule> toExplore = new ArrayList<>();
+			toExplore.add(bagRule);
+			
+			while(!toExplore.isEmpty()) {
+				toExplore = toExplore.stream()
+								.map(bag -> bag.getContent().keySet())
+								.flatMap(Set::stream)
+								.map(key -> mapRules.get(key))
+								.filter(Objects::nonNull)
+								.collect(Collectors.toList());
+				
+				for (BagRule rule : toExplore) {
+					if(rule.getColor().equals(searchingColor) && !this.canContainSearchingColorBag.contains(rule.getColor())) {
+						this.canContainSearchingColorBag.add(bagRule.getColor());
+					}
+				}
+				
+				toExplore = toExplore.stream().filter(bag -> !bag.getColor().equals(searchingColor)).collect(Collectors.toList());
+			}
+		}
+		
+		return true;
+	}
+
+	@Override
+	public String get() {
+		return String.valueOf(this.canContainSearchingColorBag.size());
+	}
+
+}
