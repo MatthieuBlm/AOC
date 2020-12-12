b0402beb8f5642cf46e5abfb4d1fc375ae68a2c6
MatthieuBlm <matthieu.bellamy@hotmail.fr>
2020-12-12 14:34:23 +0100

    Remove useless main

--- src/main/java/com/matthieu/aoc_2020/service/resolver/year_2020/Resolver12p2.java
+++ src/main/java/com/matthieu/aoc_2020/service/resolver/year_2020/Resolver12p2.java
@@ -2,7 +2,6 @@ package com.matthieu.aoc_2020.service.resolver.year_2020;
 
 import com.matthieu.aoc_2020.exception.SolveException;
 import com.matthieu.aoc_2020.model.Direction;
-import com.matthieu.aoc_2020.model.Ship;
 import com.matthieu.aoc_2020.model.WayPointShip;
 
 public class Resolver12p2 extends Resolver12p1 {
@@ -15,21 +14,5 @@ public class Resolver12p2 extends Resolver12p1 {
 		super.solve();
 		
 		return true;
-	}
-
-	
-	public static void main(String[] args) {
-		Ship s = new WayPointShip(Direction.EAST);
-		
-		s.moveForward(10);
-		System.out.println(s);
-		s.move(Direction.NORTH, 3);
-		System.out.println(s);
-		s.moveForward(7);
-		System.out.println(s);
-		s.turnRight(90);
-		System.out.println(s);
-		s.moveForward(11);
-		System.out.println(s);
 	}
 }
