5cc49187830d938185157a52845516c92a8ee03d
MatthieuBlm <matthieu.bellamy@hotmail.fr>
2021-12-11 11:54:09 +0100

    Prepare day 12

---
+++ src/main/java/com/matthieu/aoc/resolver/year_2021/Resolver12p1.java
@@ -0,0 +1,28 @@
+package com.matthieu.aoc.resolver.year_2021;
+
+import java.util.List;
+
+import com.matthieu.aoc.exception.PrepareDataException;
+import com.matthieu.aoc.exception.SolveException;
+import com.matthieu.aoc.resolver.Resolver;
+
+public class Resolver12p1 implements Resolver{
+
+	@Override
+	public void prepareData(List<String> values) throws PrepareDataException {
+		
+	}
+
+	@Override
+	public boolean solve() throws SolveException {
+		
+		return false;
+	}
+
+	@Override
+	public String get() {
+
+		return null;
+	}
+
+}
---
+++ src/main/java/com/matthieu/aoc/resolver/year_2021/Resolver12p2.java
@@ -0,0 +1,5 @@
+package com.matthieu.aoc.resolver.year_2021;
+
+public class Resolver12p2 extends Resolver12p1 {
+
+}
