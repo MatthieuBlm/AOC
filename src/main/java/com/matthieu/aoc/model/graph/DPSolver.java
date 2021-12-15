bb3103878117867e6bee97d22d2a51c1490d8775
Matthieu Bellamy <matthieu.bellamy@buawei.com>
2021-12-15 13:43:22 +0100

    Create graph solver

---
+++ src/main/java/com/matthieu/aoc/model/graph/DPSolver.java
@@ -0,0 +1,36 @@
+package com.matthieu.aoc.model.graph;
+
+import com.matthieu.aoc.model.matrix.Matrix;
+
+public class DPSolver {
+
+	private DPSolver() {}
+	
+	public static long calculateMinimumPath(Matrix<Long> cost) {
+		int width = cost.getXSize();
+		int height = cost.getYSize();
+		
+        Matrix<Long> temporaryCost = new Matrix<>(width, height, () -> 0l);
+		
+        temporaryCost.set(0, 0, cost.get(0, 0));
+		
+        // Initialize first
+        for (int x = 1; x < width; x++) {
+        	temporaryCost.set(x, 0, temporaryCost.get(x - 1, 0) + cost.get(x, 0));
+        }
+        
+        // Initialize first row
+        for (int y = 1; y < height; y++) {
+        	temporaryCost.set(0, y, temporaryCost.get(0, y - 1) + cost.get(0, y));
+        }
+        
+        // Construct rest of the temporaryCost array
+        for (int y = 1; y < height; y++) {
+        	for (int x = 1; x < width; x++) {
+                temporaryCost.set(x, y, Math.min(temporaryCost.get(x-1, y), temporaryCost.get(x, y-1)) + cost.get(x, y));
+            }
+        }
+        
+		return temporaryCost.get(width - 1, height - 1);
+	}
+}
