ef0d3b51ff3b6c8fd140b79f315ff37038242e4a
Matthieu Bellamy <matthieu.bellamy@buawei.com>
2021-12-15 13:44:59 +0100

    Resolve day 15

--- src/main/java/com/matthieu/aoc/resolver/year_2021/Resolver15p1.java
+++ src/main/java/com/matthieu/aoc/resolver/year_2021/Resolver15p1.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.matthieu.aoc.exception.PrepareDataException;
 import com.matthieu.aoc.exception.SolveException;
+import com.matthieu.aoc.model.graph.DPSolver;
 import com.matthieu.aoc.model.matrix.Matrix;
 import com.matthieu.aoc.resolver.Resolver;
 
@@ -20,7 +21,7 @@ public class Resolver15p1 implements Resolver {
 	@Override
 	public boolean solve() throws SolveException {
 		
-		this.minRisk = this.calcMinRisk(risks, this.risks.getMaxX(), this.risks.getMaxY());
+		this.minRisk = DPSolver.calculateMinimumPath(risks) - this.risks.get(0, 0);
 		
 		return true;
 	}
@@ -29,28 +30,4 @@ public class Resolver15p1 implements Resolver {
 	public String get() {
 		return String.valueOf(this.minRisk);
 	}
-
-	protected long calcMinRisk(Matrix<Long> risk, int m, int n) {
-        Matrix<Long> temporaryCost = new Matrix<>(m + 1, n + 1, () -> 0l);
- 
-        temporaryCost.set(0, 0, risk.get(0, 0));
-        
-        // Initialize first column of total cost(temporaryCost) array
-        for (int i = 1; i <= m; i++)
-            temporaryCost.set(i, 0, temporaryCost.get(i-1, 0) + risk.get(i, 0));
-
-        // Initialize first row of temporaryCost array
-        for (int j = 1; j <= n; j++)
-            temporaryCost.set(0, j, temporaryCost.get(0, j-1) + risk.get(0, j));
- 
-        // Construct rest of the temporaryCost array
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                temporaryCost.set(i, j, Math.min(temporaryCost.get(i-1, j), temporaryCost.get(i, j-1)) + risk.get(i, j));
-            }
-        }
-        
-        return temporaryCost.get(m, n) - risk.get(0, 0);
-	}
-	
 }
--- src/main/java/com/matthieu/aoc/resolver/year_2021/Resolver15p2.java
+++ src/main/java/com/matthieu/aoc/resolver/year_2021/Resolver15p2.java
@@ -3,10 +3,14 @@ package com.matthieu.aoc.resolver.year_2021;
 import java.util.List;
 
 import com.matthieu.aoc.exception.PrepareDataException;
+import com.matthieu.aoc.exception.SolveException;
+import com.matthieu.aoc.model.graph.DijkstraSolver;
+import com.matthieu.aoc.model.graph.Node;
 import com.matthieu.aoc.model.matrix.Matrix;
 
 public class Resolver15p2 extends Resolver15p1 {
 
+	private Matrix<Node> nodes;
 	
 	@Override
 	public void prepareData(List<String> values) throws PrepareDataException {
@@ -20,7 +24,7 @@ public class Resolver15p2 extends Resolver15p1 {
 					for (int x = 0; x < r.getXSize(); x++) {
 						long newRisk = r.get(x, y) + i + j;
 						
-						newRisk = newRisk % 10 + Double.valueOf(Math.floor(newRisk / 10)).longValue();
+						newRisk = newRisk % 10 + (newRisk / 10);
 						
 						risks.set(x + j * r.getXSize(), y + i * r.getYSize(), newRisk);
 
@@ -28,5 +32,25 @@ public class Resolver15p2 extends Resolver15p1 {
 				}
 			}
 		}
+		
+		this.nodes = new Matrix<>(this.risks.getXSize(), this.risks.getYSize(), () -> new Node());
+		
+		nodes.forEach(this::linkNode);
+	}
+	
+	@Override
+	public boolean solve() throws SolveException {
+		
+		DijkstraSolver.calculateShortestPathFromSource(this.nodes.get(0, 0));
+		
+		this.minRisk = this.nodes.get(this.nodes.getMaxX(), this.nodes.getMaxY()).getDistance();
+		
+		return true;
+	}
+	
+	private void linkNode(int x, int y, Node node) {
+		
+		nodes.forEachNeigthboursCross(x, y, (nx, ny, neightbourNode) -> node.addDestination(neightbourNode, this.risks.get(nx, ny).intValue()));
+		
 	}
 }
