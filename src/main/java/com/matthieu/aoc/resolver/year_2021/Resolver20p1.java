11fcbee038513b08a1ad460d1d068d90608b8e1c
Matthieu Bellamy <matthieu.bellamy@buawei.com>
2021-12-20 18:21:11 +0100

    m

--- src/main/java/com/matthieu/aoc/resolver/year_2021/Resolver20p1.java
+++ src/main/java/com/matthieu/aoc/resolver/year_2021/Resolver20p1.java
@@ -12,24 +12,41 @@ public class Resolver20p1 implements Resolver {
 
 	protected Matrix<Character> image;
 	protected String enhancementAlgorithm;
+	protected int enhancementStep;
 	
 	@Override
 	public void prepareData(List<String> values) throws PrepareDataException {
+		this.enhancementStep = 2;
 		this.enhancementAlgorithm = values.remove(0);
 		
 		values.remove(0);	// Remove empty line
 		
 		Matrix<Character> brutImage = new Matrix<>(values, s -> s.charAt(0));
 		
-		image = new Matrix<>(brutImage.getWidth() * 2, brutImage.getHeight() * 2, () -> '.');
+		image = new Matrix<>(brutImage.getWidth() * 4, brutImage.getHeight() * 4, () -> '.');
+		
+		int xShift = image.getWidth() / 4;
+		int yShift = image.getHeight() / 4;
+		brutImage.forEach((x, y, v) -> image.set(x + xShift, y + yShift, v));
 	}
 
 	@Override
 	public boolean solve() throws SolveException {
-
-		this.image.forEach((x, y, v) -> {
+		System.out.println(this.image);
+		
+		for (int i = 0; i < this.enhancementStep; i++) {
 			
-		});
+			Matrix<Character> newImage = new Matrix<>(image.getWidth(), image.getHeight(), () -> null);
+			
+			this.image.forEach((x, y, v) -> {
+				Character newC = getAlgorithmeChar(binaryToInteger(getBinary(x, y)));
+				
+				newImage.set(x, y, newC);
+			});
+			
+			this.image = newImage;
+			System.out.println(this.image);
+		}
 		
 		return true;
 	}
