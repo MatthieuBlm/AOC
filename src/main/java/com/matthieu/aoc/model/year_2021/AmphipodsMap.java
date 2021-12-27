cc58771634ee0e1b4eb3f6654cec36f0b9906169
MatthieuBlm <matthieu.bellamy@hotmail.fr>
2021-12-27 20:57:00 +0100

    Implements day 23 p 2 but not working

--- src/main/java/com/matthieu/aoc/model/year_2021/AmphipodsMap.java
+++ src/main/java/com/matthieu/aoc/model/year_2021/AmphipodsMap.java
@@ -1,5 +1,7 @@
 package com.matthieu.aoc.model.year_2021;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public final class AmphipodsMap {
@@ -23,18 +25,71 @@ public final class AmphipodsMap {
 
 	@Override
 	public int hashCode() {
-		// TODO
-		return 0;
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + Arrays.hashCode(openspace);
+		result = prime * result + ((rooms == null) ? 0 : rooms.hashCode());
+		return result;
 	}
 
 	@Override
 	public boolean equals(Object obj) {
-		// TODO
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AmphipodsMap other = (AmphipodsMap) obj;
+		if (!Arrays.equals(openspace, other.openspace))
+			return false;
+		if (rooms == null) {
+			if (other.rooms != null)
+				return false;
+		} else if (!rooms.equals(other.rooms))
+			return false;
 		return true;
 	}
 
-	
-	
-	
-	
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder("#############\n#");
+		
+		List<Character> roomA = new ArrayList<>();
+		List<Character> roomB = new ArrayList<>();
+		List<Character> roomC = new ArrayList<>();
+		List<Character> roomD = new ArrayList<>();
+		
+		roomA.addAll(this.rooms.get(0));
+		roomB.addAll(this.rooms.get(1));
+		roomC.addAll(this.rooms.get(2));
+		roomD.addAll(this.rooms.get(3));
+
+		List<List<Character>> tmpRooms = Arrays.asList(roomA, roomB, roomC, roomD);
+		
+		for (List<Character> room : tmpRooms) {
+			while(room.size() < 4)
+				room.add(0, '.');
+		}
+		
+		builder.append(this.openspace)
+				.append("#\n")
+				.append("###")
+				.append(tmpRooms.get(0).get(0)).append('#').append(tmpRooms.get(1).get(0)).append('#').append(tmpRooms.get(2).get(0)).append('#').append(tmpRooms.get(3).get(0))
+				.append("###\n");
+		
+		for (int ai = 1; ai < 4; ai++) {
+			builder.append("  ");
+			
+			for (int ri = 0; ri < 4; ri++) {
+				builder.append('#').append(tmpRooms.get(ri).get(ai));
+			}
+			
+			builder.append("#\n");
+		}
+		
+		builder.append("  #########\n");
+
+		return builder.toString();
+	}
 }
--- src/main/java/com/matthieu/aoc/resolver/year_2021/Resolver23p2.java
+++ src/main/java/com/matthieu/aoc/resolver/year_2021/Resolver23p2.java
@@ -2,36 +2,63 @@ package com.matthieu.aoc.resolver.year_2021;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import com.matthieu.aoc.exception.PrepareDataException;
 import com.matthieu.aoc.exception.SolveException;
+import com.matthieu.aoc.model.graph.DijkstraSolver;
 import com.matthieu.aoc.model.graph.Node;
+import com.matthieu.aoc.model.tuple.Duo;
 import com.matthieu.aoc.model.year_2021.AmphipodsMap;
 import com.matthieu.aoc.resolver.Resolver;
 
 public class Resolver23p2 implements Resolver {
 
 	protected Map<AmphipodsMap, Node> nodes;
+	protected AmphipodsMap initialState;
+	protected AmphipodsMap finalState;
 	
 	@Override
 	public void prepareData(List<String> values) throws PrepareDataException {
 		this.nodes = new HashMap<>();
-		
+		this.initialState = new AmphipodsMap(new char[] {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'}, Arrays.asList(List.of('C', 'D', 'D', 'D'), List.of('C', 'C', 'B', 'D'), List.of('A', 'B', 'A', 'B'), List.of('B', 'A', 'C', 'A')));
+		this.finalState	  = new AmphipodsMap(new char[] {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'}, Arrays.asList(List.of('A', 'A', 'A', 'A'), List.of('B', 'B', 'B', 'B'), List.of('C', 'C', 'C', 'C'), List.of('D', 'D', 'D', 'D')));
 	}
 
 	@Override
 	public boolean solve() throws SolveException {
 		List<AmphipodsMap> toInitialize = new ArrayList<>();
 		
-		toInitialize.add(new AmphipodsMap(new char[] {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'}, Arrays.asList(List.of('C', 'D', 'D', 'D'), List.of('C', 'C', 'B', 'D'), List.of('A', 'B', 'A', 'B'), List.of('B', 'A', 'C', 'A'))));
+		Node initialNode = new Node();
+		
+		this.nodes.put(initialState, initialNode);
+		
+		toInitialize.add(initialState);
+		
 		
 		while(!toInitialize.isEmpty()) {
 			AmphipodsMap mapToProcess = toInitialize.remove(0);
+			Node currentNode = this.nodes.get(mapToProcess);
 			
+			List<Duo<AmphipodsMap, Integer>> nextPossibleMaps = this.calcNextPossibleState(mapToProcess);
 			
+			for (Duo<AmphipodsMap, Integer> mapAndDistance : nextPossibleMaps) {
+				AmphipodsMap nextMap = mapAndDistance.a();
+				
+				// If unknown next map, create node mark map to be initialized
+				Node node = this.nodes.computeIfAbsent(nextMap, k -> {
+					toInitialize.add(nextMap);
+					return new Node();
+				});
+
+				// Setup nodes connection
+				currentNode.addDestination(node, mapAndDistance.b());
+			}
 		}
 		
 		return true;
@@ -39,52 +66,53 @@ public class Resolver23p2 implements Resolver {
 
 	@Override
 	public String get() {
-		// TODO Auto-generated method stub
-		return null;
+		DijkstraSolver.calculateShortestPathFromSource(nodes.get(initialState));
+		
+		return String.valueOf(nodes.get(finalState).getDistance());
 	}
 
 	/*			#############
-	 * index : 	#12 4 6 8 AB#
+	 * index : 	#01 3 5 7 9A#
 	 * 			### # # # ###
-	 *            #3#5#7#9#
+	 *            #2#4#6#8#
 	 */
-	private List<AmphipodsMap> calcNextPossibleStateFrom(AmphipodsMap map, int fromIndex) {
-		List<AmphipodsMap> results = new ArrayList<>();
-		
-		List<Integer> possibleIndexes = this.getPossibleIndexesFrom(map, fromIndex);
-		
-		return results;
+	private List<Duo<AmphipodsMap, Integer>> calcNextPossibleState(AmphipodsMap map) {
+		return IntStream.range(0, 11)
+							.mapToObj(i -> new Duo<>(i, this.getPossibleIndexesFrom(map, i)))
+							.filter(duo -> !duo.b().isEmpty())
+							.flatMap(duo -> duo.b().stream().map(indexToMove -> new Duo<>(this.moveAmphipod(map, duo.a(), indexToMove.a()), indexToMove.b())))
+							.collect(Collectors.toList());
 	}
 	
 	private AmphipodsMap moveAmphipod(AmphipodsMap map, int from, int to) {
 		AmphipodsMap newMap = this.clone(map);
 		char toMove;
 		
-		if(from <= 2 || from == 4 || from == 6 || from == 8 || from >= 10) {
+		if(from < 2 || from == 3 || from == 5 || from == 7 || from >= 9) {
 			toMove = newMap.getOpenspace()[from];
 			newMap.getOpenspace()[from] = '.';
-		} else if(from == 3) {
+		} else if(from == 2) {
 			toMove = newMap.getRooms().get(0).remove(0);
-		} else if(from == 5) {
+		} else if(from == 4) {
 			toMove = newMap.getRooms().get(1).remove(0);
-		} else if(from == 7) {
+		} else if(from == 6) {
 			toMove = newMap.getRooms().get(2).remove(0);
-		} else if(from == 9) {
+		} else if(from == 8) {
 			toMove = newMap.getRooms().get(3).remove(0);
 		} else {
 			throw new IllegalArgumentException("Unvailable index from "+ from);
 		}
 		
 		
-		if(to <= 2 || to == 4 || to == 6 || to == 8 || to >= 10) {
+		if(to < 2 || to == 3 || to == 5 || to == 7 || to >= 9) {
 			newMap.getOpenspace()[to] = toMove;
-		} else if(to == 3) {
+		} else if(to == 2) {
 			newMap.getRooms().get(0).add(0, toMove);
-		} else if(to == 5) {
+		} else if(to == 4) {
 			newMap.getRooms().get(1).add(0, toMove);
-		} else if(to == 7) {
+		} else if(to == 6) {
 			newMap.getRooms().get(2).add(0, toMove);
-		} else if(to == 9) {
+		} else if(to == 8) {
 			newMap.getRooms().get(3).add(0, toMove);
 		} else {
 			throw new IllegalArgumentException("Unvailable index to "+ to);
@@ -111,38 +139,99 @@ public class Resolver23p2 implements Resolver {
 	}
 	
 	private boolean isFreeSpace(AmphipodsMap map, int i) {
-		if(i == 1 || i == 2 || i == 4 || i == 6 || i == 8 || i == 10 || i == 11) {
+		if(i == 0 || i == 1 || i == 3 || i == 5 || i == 7 || i == 9 || i == 10) {
 			return map.getOpenspace()[i] == '.';
-		} else if(i == 3){
+		} else if(i == 2){
 			return map.getRooms().get(0).size() < 4;
-		} else if(i == 5){
+		} else if(i == 4){
 			return map.getRooms().get(1).size() < 4;
-		} else if(i == 7){
+		} else if(i == 6){
 			return map.getRooms().get(2).size() < 4;
-		} else if(i == 9){
+		} else if(i == 8){
 			return map.getRooms().get(3).size() < 4;
 		}
 		
 		return false;
 	}
 	
-	private List<Integer> getPossibleIndexesFrom(AmphipodsMap map, int index) {
+	// Duo<Index, Distance>
+	private List<Duo<Integer, Integer>> getPossibleIndexesFrom(AmphipodsMap map, int i) {
+		// If i is not an amphipod
+		if((isInOpenspace(i) && map.getOpenspace()[i] == '.') || (isInRoom(i) && getRoom(map, i).isEmpty()))
+			return Collections.emptyList();
+		
+		
 		List<Integer> possibleIndexes = new ArrayList<>();
-		boolean isFree;
-		int indexToTest = index;
-
+		int indexToTest = i;
+		boolean openspaceBlocked = false;
+		
 		// Forward
-		while(isFree = this.isFreeSpace(map, indexToTest++) && indexToTest <= 11) {
-			possibleIndexes.add(indexToTest);
+		while(++indexToTest < 11 && !openspaceBlocked) {
+			
+			if(this.isFreeSpace(map, indexToTest))
+				possibleIndexes.add(indexToTest);
+			
+			else if(indexToTest == 0 || indexToTest == 1 || indexToTest == 3 || indexToTest == 5 || indexToTest == 7 || indexToTest == 9 || indexToTest == 10)
+				openspaceBlocked = true;
 		}
 
-		indexToTest = index;
+		indexToTest = i;
+		openspaceBlocked = false;
 		
 		// Backward
-		while(isFree = this.isFreeSpace(map, indexToTest--) && indexToTest >= 0) {
-			possibleIndexes.add(indexToTest);
+		while(--indexToTest >= 0 && !openspaceBlocked) {
+			
+			if(this.isFreeSpace(map, indexToTest))
+				possibleIndexes.add(indexToTest);
+			
+			else if(indexToTest == 0 || indexToTest == 1 || indexToTest == 3 || indexToTest == 5 || indexToTest == 7 || indexToTest == 9 || indexToTest == 10)
+				openspaceBlocked = true;
 		}
+
+		// Calculate distances
+		return possibleIndexes.stream().map(to -> {
+			int distance = Math.abs(to - i);
+			
+			if(isInRoom(i))
+				distance += 4 - getRoom(map, i).size() + 1;
+			if(isInRoom(to))
+				distance += 4 - getRoom(map, to).size();
+			
+			distance *= this.getAmphipodMoveCost(this.getAmphipod(map, i));
+			
+			return new Duo<>(to, distance);
+		}).collect(Collectors.toList());
+	}
+	
+	private boolean isInOpenspace(int index) {
+		return index == 0 || index == 1 || index == 3 || index == 5 || index == 7 || index == 9 || index == 10;
+	}
+	
+	private boolean isInRoom(int index) {
+		return !this.isInOpenspace(index);
+	}
+	
+	private List<Character> getRoom(AmphipodsMap map, int index) {
+		return map.getRooms().get(index / 2 - 1);
+	}
+	
+	private char getAmphipod(AmphipodsMap map, int index) {
+		if(isInOpenspace(index))
+			return map.getOpenspace()[index];
+
+		return this.getRoom(map, index).get(0);
+	}
+	
+	private int getAmphipodMoveCost(char a) {
+		if(a == 'A')
+			return 1;
+		if(a == 'B')
+			return 10;
+		if(a == 'C')
+			return 100;
+		if(a == 'D')
+			return 1000;
 		
-		return possibleIndexes;
+		throw new IllegalArgumentException("Unknown amphipod '"+ a +"'");
 	}
 }
