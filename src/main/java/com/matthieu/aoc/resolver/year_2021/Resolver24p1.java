046930639551bfeb9e64b7d84b4a1836ac2f5170
Matthieu Bellamy <matthieu.bellamy@buawei.com>
2022-01-04 18:47:08 +0100

    Optimized brut force V1

--- src/main/java/com/matthieu/aoc/resolver/year_2021/Resolver24p1.java
+++ src/main/java/com/matthieu/aoc/resolver/year_2021/Resolver24p1.java
@@ -1,150 +1,91 @@
 package com.matthieu.aoc.resolver.year_2021;
 
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import com.matthieu.aoc.exception.PrepareDataException;
 import com.matthieu.aoc.exception.SolveException;
-import com.matthieu.aoc.model.I;
+import com.matthieu.aoc.model.year_2021.Base9Digit;
 import com.matthieu.aoc.resolver.Resolver;
 
 public class Resolver24p1 implements Resolver {
 
-	protected Map<Character, Integer> memory;
+	protected static final int[] xIncrements = 	new int[] {10, 15, 14, 15, -8, 10, 16, -4, 11, -3, 12, -7, -15, -7};
+	protected static final int[] yIncrements = 	new int[] {2,  16,  9, 0,   1, 12,  6,  6,  3,  5,  9,  3,   2,  3};
+	protected static final int[] zDivider = 	new int[] {1,  1,   1, 1,  26,  1, 26, 26,  1, 26,  1, 26,  26, 26};
+	
 	protected List<String> program;
+	protected boolean findMax;
+	protected Base9Digit digits;
 	
 	@Override
 	public void prepareData(List<String> values) throws PrepareDataException {
-		this.memory = new HashMap<>();
 		this.program = values;
+		this.findMax = true;
+		this.digits = new Base9Digit("99999999999999");
 	}
 
 	@Override
 	public boolean solve() throws SolveException {
-		char[] number = "12345678912345".toCharArray();
-		
-		// TODO Auto-generated method stub
-		return false;
-	}
 
-	@Override
-	public String get() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		for(int v = 0; v < 100_000_000; v++) {
+//		while(true) {
+			int z = 0;
+			
+			for (int i = 0; i < 14; i++) {
+				if(i >= 11 && z > 17576)
+					break;
+				
+				z =  doCycleVerbose(digits.get(i), xIncrements[i], yIncrements[i], zDivider[i], z);
+			}
 
-	
-	protected void inp(char var, int value) {
-		this.memory.put(var, value);
-	}
-	
-	protected void add(char a, String b) {
-		int valA = this.memory.get(a);
-		int valB = this.getValue(b);
-		
-		int res = valA + valB;
-		
-		this.memory.put(a, res);
-		
-	}
-	
-	protected void mod(char a, String b) {
-		int valA = this.memory.get(a);
-		int valB = this.getValue(b);
-		
-		int res = valA % valB;
-		
-		this.memory.put(a, res);
-	}
-	
-	protected void mul(char a, String b) {
-		int valA = this.memory.get(a);
-		int valB = this.getValue(b);
-
-		int res = valA * valB;
-		
-		this.memory.put(a, res);
-	}
-	
-	protected void div(char a, String b) {
-		int valA = this.memory.get(a);
-		int valB = this.getValue(b);
-		
-		int res = valA / valB;
-
-		this.memory.put(a, res);
-	}
-	
-	protected void eql(char a, String b) {
-		int valA = this.memory.get(a);
-		int valB = this.getValue(b);
-		
-		int res = valA == valB ? 1 : 0;
-
-		this.memory.put(a, res);
-	}
-	
-	protected int getValue(String param) {
-		return this.isDigit(param) ? Integer.valueOf(param) : this.memory.get(param.charAt(0));
-	}
-	
-	protected boolean isDigit(String s) {
-		for(char c : s.toCharArray()) {
-			if(c < '0' || c > '9')
-				return false;
+			if(z == 0) {
+				break;
+			}
+			
+			if(this.findMax)
+				digits.decr();
+			else
+				digits.incr();
 		}
 		
 		return true;
 	}
-	
-	public static void main(String[] args) {
-		int[] var = new int[] {0, 0, 0, 0};
-		
-		
-		var[0] = 2;
-		
-		var = doCycle(var[0], var[1], var[2], var[3]);
-		
-		var[0] = 6;
-		
-		var = doCycle(var[0], var[1], var[2], var[3]);
-		
-		System.out.println(Arrays.toString(var));
-		
-		int x = 19;
-		int w = 19;
-		
-		x = x == w ? 1 : 0;
-		x = x == 0 ? 1 : 0;
-		
-		System.out.println(x);
-		
-		x = 19;
-		w = 19;
-		
-		x = x != w ? 1 : 0;
 
-		System.out.println(x);
+	@Override
+	public String get() {
+		return this.digits.toString();
 	}
 	
-	public static int[] doCycle(int w, int x, int y, int z) {
-		x = z;
+	public static void main(String[] args) {
+//		Base9Digit digits = new Base9Digit("99399795999899");
+//		Base9Digit digits = new Base9Digit("99991795999899");
+//		Base9Digit digits = new Base9Digit("99999985999899");
+		Base9Digit digits = new Base9Digit("99999995999999");
+		
+		int z = 0;
+		for (int i = 0; i < 14; i++) {
+			z =  doCycleVerbose(digits.get(i), xIncrements[i], yIncrements[i], zDivider[i], z);
+		}
+		
+		System.out.println(z);
+	}
+	
+	public static int[] doCycle(int w, int xIncr, int yIncr, int zDiv, int z) {
+		int x = z;
 		x %= 26;
-		z /= 1;		// 1 or 26
-		x += 10;	// variable
+		z /= zDiv;	// 1 or 26
+		x += xIncr;	// variable
 		
 		x = x != w ? 1 : 0;
 		
-		y = 25;
+		int y = 25;
 		y *= x;
 		y++;
 		
 		z *= y;
 		
 		y = w;
-		y += 2;	// variable
+		y += yIncr;	// variable
 		y *= x;
 		z += y;
 		
@@ -153,27 +94,19 @@ public class Resolver24p1 implements Resolver {
 	
 	public static int doCycleVerbose(int input, int z) {
 		
-		int remainder = z % 26;
-		int q = z / 26; // Or 1
+		int remainder = (z % 26) + 10; // "+10" or whatever
+		int q = z / 1; 		// Or 26
 		
-		remainder += -7; // Or whatever
-		
-		int x = remainder == input ? 1 : 0;
-		x = x == 0 ? 1 : 0;
-		
-		int y = 25;
-		y *= x;
-		y++;
-		
-		int res = q * y;
-		
-		y = input;
-		y += 3; // or Whatever
-		
-		y *= x;
-		
-		res += y;
-			
-		return res;
+		if(remainder != input)
+			return q * 26 + input + 2; 	// "+2" or Whatever
+		else
+			return q;
+	}
+	
+	public static int doCycleVerbose(int input, int xIncrement, int yIncrement, int zDivider, int z) {
+		if((z % 26) + xIncrement == input)
+			return z / zDivider;
+		else
+			return z / zDivider * 26 + input + yIncrement;
 	}
 }
