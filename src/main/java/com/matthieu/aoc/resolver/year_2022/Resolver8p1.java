a3c078a8114e0060a9f3c7f1fe895f23cd8f307a
Matthieu Bellamy <matthieu.bellamy@buawei.com>
2022-12-08 13:37:31 +0100

    Optim part 1 day 8

--- src/main/java/com/matthieu/aoc/resolver/year_2022/Resolver8p1.java
+++ src/main/java/com/matthieu/aoc/resolver/year_2022/Resolver8p1.java
@@ -1,36 +1,32 @@
 package com.matthieu.aoc.resolver.year_2022;
 
+import java.util.Arrays;
 import java.util.List;
 
 import com.matthieu.aoc.exception.PrepareDataException;
 import com.matthieu.aoc.exception.SolveException;
 import com.matthieu.aoc.model.matrix.Matrix;
+import com.matthieu.aoc.model.tuple.Duo;
 import com.matthieu.aoc.resolver.Resolver;
 
 public class Resolver8p1 implements Resolver {
 	
 	protected Matrix<Integer> grid;
 	protected Matrix<Boolean> visibleTrees;
+	protected List<Duo<Integer, Integer>> directions;
 	
 	@Override
 	public void prepareData(List<String> values) throws PrepareDataException {
 		this.grid = new Matrix<>(values, Integer::parseInt);
 		this.visibleTrees = new Matrix<>(grid.getWidth(), grid.getHeight(), () -> Boolean.FALSE);
+		this.directions = Arrays.asList(new Duo<>(0, 1), new Duo<>(1, 0), new Duo<>(0, -1), new Duo<>(-1, 0));
 	}
 
 	@Override
 	public boolean solve() throws SolveException {
-		// Edges
-		for (int x = 0; x < grid.getWidth(); x++) this.visibleTrees.set(x, 0, Boolean.TRUE);
-		for (int x = 0; x < grid.getWidth(); x++) this.visibleTrees.set(x, grid.getHeight() - 1, Boolean.TRUE);
-		for (int y = 0; y < grid.getHeight(); y++) this.visibleTrees.set(0, y, Boolean.TRUE);
-		for (int y = 0; y < grid.getHeight(); y++) this.visibleTrees.set(grid.getWidth() - 1, y, Boolean.TRUE);
-		
-		grid.forEach((x, y, t) -> {
-			if(x > 0 && x < grid.getWidth() - 1 && y > 0 && y < grid.getHeight() - 1) {
-				if(isVisible(x, y, t)) {
-					this.visibleTrees.set(x, y, Boolean.TRUE);
-				}
+		grid.forEach((x, y, h) -> {
+			if(isVisible(x, y, h)) {
+				this.visibleTrees.set(x, y, Boolean.TRUE);
 			}
 		});
 		
@@ -43,49 +39,22 @@ public class Resolver8p1 implements Resolver {
 	}
 	
 	protected boolean isVisible(int x, int y, int height) {
-		
-		return isVisibleFromTop(x, y, height) || isVisibleFromRight(x, y, height) ||
-				isVisibleFromBottom(x, y, height) || isVisibleFromLeft(x, y, height);
+		return directions.stream().anyMatch(d -> isVisibleFrom(x, y, height, d));
 	}
 	
-	protected boolean isVisibleFromLeft(int x, int y, int height) {
-		for (int i = 0; i < x; i++) {
-			if(this.grid.get(i, y) >= height) {
+	protected boolean isVisibleFrom(int x,int y, int height, Duo<Integer, Integer> direction) {
+		Integer neightbourHeight;
+		Duo<Integer, Integer> pos = new Duo<>(x + direction.x(), y + direction.y());
+		
+		while((neightbourHeight = this.grid.getQuietly(pos.x(), pos.y())) != null){
+			pos.x(pos.x() + direction.x());
+			pos.y(pos.y() + direction.y());
+			
+			if(neightbourHeight >= height) {
 				return false;
 			}
 		}
 		
 		return true;
 	}
-	
-	protected boolean isVisibleFromRight(int x, int y, int height) {
-		for (int i = grid.getWidth() - 1; i > x; i--) {
-			if(this.grid.get(i, y) >= height) {
-				return false;
-			}
-		}
-		
-		return true;
-	}
-
-	protected boolean isVisibleFromTop(int x, int y, int height) {
-		for (int i = 0; i < y; i++) {
-			if(this.grid.get(x, i) >= height) {
-				return false;
-			}
-		}
-		
-		return true;
-	}
-
-	protected boolean isVisibleFromBottom(int x, int y, int height) {
-		for (int i = grid.getHeight() - 1; i > y; i--) {
-			if(this.grid.get(x, i) >= height) {
-				return false;
-			}
-		}
-		
-		return true;
-	}
-
 }
