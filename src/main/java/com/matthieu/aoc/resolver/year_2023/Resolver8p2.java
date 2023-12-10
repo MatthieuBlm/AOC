43d9f170d826685ff74fccea4affe7a136831a0d
Matthieu Bellamy <matthieu.bellamy@buawei.com>
2023-12-10 18:10:12 +0100

    Optimise day 8

--- src/main/java/com/matthieu/aoc/resolver/year_2023/Resolver8p2.java
+++ src/main/java/com/matthieu/aoc/resolver/year_2023/Resolver8p2.java
@@ -1,8 +1,6 @@
 package com.matthieu.aoc.resolver.year_2023;
 
-import java.math.BigInteger;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -10,53 +8,41 @@ import com.matthieu.aoc.service.Calculator;
 
 public class Resolver8p2 extends Resolver8p1 {
 
-	private BigInteger optimizedStep;
+	private long optimizedStep;
 	
 	@Override
 	public boolean solve() throws Exception {
 		int instructionIndex = 0;
-		List<List<String>> positions = this.nodes.keySet().stream()
+		List<String> positions = this.nodes.keySet().stream()
 				.filter(key -> key.endsWith("A"))
-				.map(key -> new ArrayList<>(Arrays.asList(key)))
 				.collect(Collectors.toCollection(ArrayList::new));
 		
 		while(!positions.isEmpty()) {
 			for (int i = 0; i < positions.size(); i++) {
-				List<String> positionHistory = positions.get(i);
-				
-				positionHistory.add(this.getNextNode(getHead(positionHistory), instructionIndex));
+				positions.set(i, this.getNextNode(positions.get(i), instructionIndex));
 			}
 			
 			steps++;
 			instructionIndex = (int) (steps % instructions.length);
 			
 			for (int i = 0; i < positions.size(); i++) {
-				String head = getHead(positions.get(i));
 				// Lap done
-				if(head.endsWith("Z")) {
-					int loopSize = positions.get(i).size() - 1;
-					
-					optimizedStep = optimizedStep == null ? BigInteger.valueOf(loopSize) : Calculator.bigLeastCommonMultiple(BigInteger.valueOf(loopSize), optimizedStep);
+				if(positions.get(i).endsWith("Z")) {
+					optimizedStep = optimizedStep == 0 ? steps : Calculator.leastCommonMultiple(steps, optimizedStep);
 					
 					positions = positions.stream()
-							.filter(history -> !getHead(history).endsWith("Z"))
+							.filter(pos -> !pos.endsWith("Z"))
 							.collect(Collectors.toCollection(ArrayList::new));
-					
 					break;
 				}
 			}
 		}
-		
 		return true;
 	}
 	
 	@Override
 	public String get() {
-		return this.optimizedStep.toString();
-	}
-	
-	private static String getHead(List<String> list) {
-		return list.get(list.size() - 1);
+		return this.optimizedStep + "";
 	}
 	
 	private String getNextNode(String node, int instructionIndex) {
@@ -69,24 +55,5 @@ public class Resolver8p2 extends Resolver8p1 {
 		} else {
 			throw new IllegalStateException();
 		}
-	}
-	
-	public static void main(String[] args) {
-		List<BigInteger> numbers = Arrays.asList(BigInteger.valueOf(12878), 
-				BigInteger.valueOf(14628), 
-				BigInteger.valueOf(16225), 
-				BigInteger.valueOf(18492), 
-				BigInteger.valueOf(19596), 
-				BigInteger.valueOf(21567));
-		
-		List<BigInteger> carlos = Arrays.asList(BigInteger.valueOf(16271l), 
-				BigInteger.valueOf(24253l), 
-				BigInteger.valueOf(14429l), 
-				BigInteger.valueOf(22411l), 
-				BigInteger.valueOf(18727l), 
-				BigInteger.valueOf(20569l));
-		
-		System.out.println(carlos.stream().reduce(Calculator::bigLeastCommonMultiple).get());
-		
 	}
 }
