cc1f0acca00e9cdfac55587c71ee3d560a067ab4
Matthieu Bellamy <matthieu.bellamy@buawei.com>
2023-12-18 22:52:05 +0100

    Start to resove day 16

--- src/main/java/com/matthieu/aoc/model/matrix/Cell.java
+++ src/main/java/com/matthieu/aoc/model/matrix/Cell.java
@@ -1,3 +1,42 @@
 package com.matthieu.aoc.model.matrix;
 
-public record Cell<T>(int x, int y, T value) {}
+public class Cell<T> {
+
+	private int x;
+	private int y;
+	private T value;
+	
+	public Cell(int x, int y, T value) {
+		this.x = x;
+		this.y = y;
+		this.value = value;
+	}
+	
+	public int x() {
+		return x;
+	}
+	
+	public void x(int x) {
+		this.x = x;
+	}
+	
+	public int y() {
+		return y;
+	}
+	
+	public void y(int y) {
+		this.y = y;
+	}
+	
+	public T value() {
+		return this.value;
+	}
+	
+	public void value(T value) {
+		this.value = value;
+	}
+	@Override
+	public String toString() {
+		return "Cell [x=" + x + ", y=" + y + ", value=" + value + "]";
+	}
+}
