1c1088b9ed98fcd0602a5a84c3d58d15349943d6
Matthieu Bellamy <matthieu.bellamy@buawei.com>
2024-11-15 14:14:18 +0100

    Try to build simplified graph for part 2

--- src/main/java/com/matthieu/aoc/resolver/year_2023/Resolver23p2.java
+++ src/main/java/com/matthieu/aoc/resolver/year_2023/Resolver23p2.java
@@ -1,70 +1,63 @@
 package com.matthieu.aoc.resolver.year_2023;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import com.matthieu.aoc.exception.PrepareDataException;
+import com.matthieu.aoc.model.graph.Node;
 import com.matthieu.aoc.model.matrix.Matrix;
 import com.matthieu.aoc.model.tuple.Duo;
 
 public class Resolver23p2 extends Resolver23p1 {
 
-	private List<Matrix<Long>> allDistances;
-	
-	 @Override
-	 public void prepareData(List<String> values) throws PrepareDataException {
-		 super.prepareData(values);
-		 this.allDistances = new ArrayList<>();
-	 }
+    private Matrix<Node> graph;
+
+    @Override
+    public void prepareData(List<String> values) throws PrepareDataException {
+        super.prepareData(values);
+        this.graph = new Matrix<>(this.map.getWidth(), this.map.getHeight(), () -> null);
+
+        buildGraph();
+    }
 	 
 	@Override
 	public boolean solve() throws Exception {
-		calcMaxDistanceOfNeighbourgs(destination.x(), destination.y() - 1, destination.x(), destination.y(), super.distances);
 	    return true;
 	}
 	
-	protected void calcMaxDistanceOfNeighbourgs(int x, int y, int fromX, int fromY, Matrix<Long> distances) {
-    	long distance = distances.get(x, y);
-    	
-    	
-    	if(x == start.x() && y == start.y()) {
-    		return;
-    	}
-    	
-    	List<Duo<Integer, Integer>> neightbours = Matrix.getNeigthboursCrossCoords(x, y).stream()
-    			.filter(coords -> !(coords.x() == fromX && coords.y() == fromY))
-    			.toList();
-    	
-    	
-    	for (Duo<Integer, Integer> neightbour : neightbours) {
-    		char cell = map.get(neightbour.x(), neightbour.y());
-    		
-    		Matrix<Long> distanceToUse;
-    		
-    		if(neightbours.size() == 1) {
-    			distanceToUse = distances;
-    		} else {
-    			distanceToUse = clone(distances);
-    			allDistances.remove(distances);
-    			allDistances.add(distanceToUse);
-    		}
-			
-    		Long neightbourDistance = distanceToUse.get(neightbour.x(), neightbour.y());
-    		
-    		if(cell != '#' && neightbourDistance == 0) {
-    			distanceToUse.set(neightbour.x(), neightbour.y(), Math.max(neightbourDistance, distance + 1));
-    			this.calcMaxDistanceOfNeighbourgs(neightbour.x(), neightbour.y(), x, y, distanceToUse);
-    		}
-		}
-    	
-    }
-	
     @Override
     public String get() {
-    	return this.allDistances.stream().mapToLong(d -> d.get(1, 0)).max().getAsLong() + "";
+        return "";
     }
 	
-	private Matrix<Long> clone(Matrix<Long> distances) {
-		return new Matrix<>(distances.getWidth(), distances.getHeight(), distances::get);
-	}
+
+    private void buildGraph() {
+        this.graph.set(1, 0, new Node("Start"));
+        this.graph.set(this.graph.getMaxX() - 1, this.graph.getMaxY(), new Node("Destination"));
+
+        browseMaze(1, 0, 1, 0, this.graph.get(1, 0), 0);
+    }
+
+    private void browseMaze(int x, int y, int fromX, int fromY, Node origin, int distanceFromOrigin) {
+        List<Duo<Integer, Integer>> neightbours = Matrix.getNeigthboursCrossCoords(x, y).stream()
+                .filter(coords -> coords.x() >= 0 && coords.x() < this.map.getWidth()) // Filter out of bounds
+                .filter(coords -> coords.y() >= 0 && coords.y() < this.map.getHeight()) // Filter out of bounds
+                .filter(coords -> !(coords.x() == fromX && coords.y() == fromY)) // Filter from
+                .filter(coords -> this.map.get(coords.x(), coords.y()) != '#') // Filter walls
+                .toList();
+
+        if (neightbours.size() == 1) {
+            browseMaze(neightbours.get(0).x(), neightbours.get(0).y(), x, y, origin, distanceFromOrigin + 1);
+
+        } else if (neightbours.size() > 1) {
+            Node junction = new Node("Junction");
+
+            origin.addDestination(junction, distanceFromOrigin);
+            junction.addDestination(origin, distanceFromOrigin);
+
+            for (Duo<Integer, Integer> neightbour : neightbours) {
+                browseMaze(neightbour.x(), neightbour.y(), x, y, junction, 1);
+            }
+        }
+    }
+
 }
