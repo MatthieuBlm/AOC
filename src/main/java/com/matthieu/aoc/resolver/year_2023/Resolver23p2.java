62ec43833a9b5de4161eb9797e5a5b1952409d8f
Matthieu Bellamy <matthieu.bellamy@buawei.com>
2024-11-18 19:32:54 +0100

    Try to browse simplified graph

--- src/main/java/com/matthieu/aoc/resolver/year_2023/Resolver23p2.java
+++ src/main/java/com/matthieu/aoc/resolver/year_2023/Resolver23p2.java
@@ -1,6 +1,9 @@
 package com.matthieu.aoc.resolver.year_2023;
 
+import java.util.HashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -13,26 +16,82 @@ import com.matthieu.aoc.model.tuple.Duo;
 public class Resolver23p2 extends Resolver23p1 {
 
     private Matrix<Node> graph;
+    private int junctionCounter = 1;
+    private List<Node> nodes;
+    private Map<LinkedHashSet<Node>, Long> paths;
 
     @Override
     public void prepareData(List<String> values) throws PrepareDataException {
         super.prepareData(values);
         this.graph = new Matrix<>(this.map.getWidth(), this.map.getHeight(), () -> null);
+        this.paths = new HashMap<>();
 
         buildGraph();
+
+        nodes = this.graph.cellStream().map(Cell::value).filter(Objects::nonNull).toList();
     }
 	 
 	@Override
 	public boolean solve() throws Exception {
-        List<Node> nodes = this.graph.cellStream().map(Cell::value).filter(Objects::nonNull).toList();
+        Node start = this.nodes.get(0);
+
+        if (!"Start".equals(start.getName())) {
+            throw new IllegalStateException("Start node not found");
+        }
+
+        Map.Entry<Node, Integer> node = start.getAdjacentNodes().entrySet().iterator().next();
+
+        LinkedHashSet<Node> path = new LinkedHashSet<>();
+        path.add(start);
+        path.add(node.getKey());
+        paths.put(path, (long) node.getValue());
+
+        browseGraph(node.getKey(), start, path);
+
 	    return true;
 	}
 	
     @Override
     public String get() {
-        return "";
+        return this.paths.values().stream().mapToLong(Long::longValue).max().orElseThrow() + "";
     }
 	
+    private void browseGraph(Node currentNode, Node fromNode, LinkedHashSet<Node> path) {
+        if (currentNode.getAdjacentNodes().size() == 2) {
+            // Only one way to go
+            Map.Entry<Node, Integer> nextNode = currentNode.getAdjacentNodes().entrySet().stream()
+                    .filter(entry -> !entry.getKey().equals(fromNode)) // Avoid going back
+                    .findFirst()
+                    .orElseThrow();
+
+            // If not already in path
+            if (!path.contains(nextNode.getKey())) {
+
+                path.add(nextNode.getKey());
+                paths.put(path, paths.get(path) + nextNode.getValue());
+            }
+
+        } else if (currentNode.getAdjacentNodes().size() > 2) {
+
+            for (Map.Entry<Node, Integer> node : currentNode.getAdjacentNodes().entrySet()) {
+                if (node.getKey().equals(fromNode) || path.contains(node.getKey())) {
+                    continue;
+                }
+
+                LinkedHashSet<Node> newPath = new LinkedHashSet<>(path);
+                newPath.add(node.getKey());
+                long distance = paths.remove(path);
+                paths.put(newPath, distance + node.getValue());
+
+                browseGraph(node.getKey(), currentNode, newPath);
+            }
+
+        } else {
+            System.out.println("End of path");
+            // TODO
+        }
+
+    }
 
     private void buildGraph() {
         this.graph.set(1, 0, new Node("Start"));
@@ -58,7 +117,7 @@ public class Resolver23p2 extends Resolver23p1 {
                 browseMaze(neightbours.get(0).x(), neightbours.get(0).y(), x, y, origin, distanceFromOrigin + 1);
 
         } else if (neightbours.size() > 1) {
-            Node junction = Optional.ofNullable(graph.get(x, y)).orElseGet(() -> new Node("Junction"));
+            Node junction = Optional.ofNullable(graph.get(x, y)).orElseGet(() -> new Node("" + junctionCounter++));
 
             linkNodes(origin, junction, distanceFromOrigin);
 
