86143ffb7ab2a52af52db9d0c53d199d2f0667b0
MatthieuBlm <matthieu.bellamy@hotmail.fr>
2024-12-16 23:40:04 +0100

    Resolve day 15 and 16 part 1

--- src/main/java/com/matthieu/aoc/resolver/year_2024/Resolver15p1.java
+++ src/main/java/com/matthieu/aoc/resolver/year_2024/Resolver15p1.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.matthieu.aoc.exception.PrepareDataException;
+import com.matthieu.aoc.model.Direction;
 import com.matthieu.aoc.model.Point;
+import com.matthieu.aoc.model.matrix.Cell;
 import com.matthieu.aoc.model.matrix.CharMatrix;
 import com.matthieu.aoc.resolver.Resolver;
 
@@ -12,6 +14,7 @@ public class Resolver15p1 implements Resolver {
 
     protected CharMatrix map;
     protected List<Character> moves;
+    protected Point robot;
 
     @Override
     public void prepareData(List<String> values) throws PrepareDataException {
@@ -32,29 +35,62 @@ public class Resolver15p1 implements Resolver {
                 moves.add(chars[i]);
             }
         }
+        
+        // Find robot
+        Cell<Character> robotCell = map.cellStream().filter(cell -> cell.value() == '@').findAny().orElseThrow(); 
+        this.robot = new Point(robotCell.x(), robotCell.y());
+        this.map.set(robot, '.');
     }
 
     @Override
     public boolean solve() throws Exception {
 
+    	moves.forEach(move -> move(robot, Direction.fromCharacter(move).getVector()));
+    	
         return true;
     }
 
     @Override
     public String get() {
-        return null;
+        return this.map.cellStream()
+        		.filter(cell -> cell.value() == 'O')
+        		.mapToLong(cell -> cell.x() + cell.y() * 100l)
+        		.sum() + "";
     }
 
     protected void move(Point robot, Point direction) {
         Point position = robot;
         Point nextCell = addOneMove(position, direction);
 
-        while (map.get(nextCell) != '#' && map.get(nextCell) != '.') {
-            Character cellValue = map.get(nextCell);
+        if(map.get(nextCell) == 'O') {
+        	pushBox(nextCell, direction);
+        }
+        
+        if(map.get(nextCell) == '.') {
+        	robot.setX(nextCell.x());
+        	robot.setY(nextCell.y());
         }
     }
 
-    protected Point addOneMove(Point from, Point direction) {
+    protected boolean pushBox(Point box, Point direction) {
+    	Point nextCell = this.addOneMove(box, direction);
+    	
+		if(map.get(nextCell) == '.' || (map.get(nextCell) == 'O' && pushBox(nextCell, direction))) {
+			map.set(box, '.');
+			map.set(nextCell, 'O');
+			return true;
+		}
+		
+		return false;
+	}
+
+	protected Point addOneMove(Point from, Point direction) {
         return new Point(from.x() + direction.x(), from.y() + direction.y());
     }
+	
+	protected String printMap() {
+		CharMatrix clone = new CharMatrix(map.getWidth(), map.getHeight(), map::get);
+		clone.set(robot, '@');
+		return clone.toString();
+	}
 }
